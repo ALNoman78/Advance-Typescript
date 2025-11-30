d9239e8e7814cd2270c3d28db05eac90dca0b83e
ALNoman78 <abdullaalno078@gmail.com>
2025-11-30 22:55:52 +0600

    work in progress

--- src/interface.ts
+++ src/interface.ts
@@ -2,24 +2,50 @@
 
 type User = {
     name: string;
-    age : number
+    age: number
 }
 
-const user1: User ={
-    name : "moule",
-    age : 21
-} 
+const user1: User = {
+    name: "moule",
+    age: 21
+}
 
 type Role = {
-    role : 'admin' | 'user'
+    role: 'admin' | 'user'
 }
 
 type UserWithRole = User & Role
 
-const user2 : UserWithRole = {
-    name : "Mr x",
-    age : 22,
-    role : 'admin',
+const user2: UserWithRole = {
+    name: "Mr x",
+    age: 22,
+    role: 'admin',
 }
 
-// todo 
+console.log(user2)
+
+// todo Interface in Typescript
+
+//! interface is start with second bracket, we can't use interface for primitive data type
+// interface only work for Object type : array , object, function
+
+interface IUser {
+    name: string;
+    age: number
+}
+
+
+const user3: IUser = {
+    name: "Noman",
+    age: 21
+}
+
+type IsAdmin = boolean
+
+const isAdmin: IsAdmin = false
+
+//  use interface
+
+interface IUserWithRole extends IUser {
+    role: "admin" | 'user'
+}
