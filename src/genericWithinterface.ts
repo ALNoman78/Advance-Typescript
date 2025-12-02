cceb684f0d7f74622c8c07192dc37aee00b54481
ALNoman78 <abdullaalno078@gmail.com>
2025-12-02 22:51:38 +0600

    interface with generic

--- src/genericWithinterface.ts
+++ src/genericWithinterface.ts
@@ -3,14 +3,32 @@
 type GenericArray<T> = Array<T>
 
 type User = {
-    name : string,
-    age : number
+    name: string,
+    age: number
 }
 
 
-const userList : GenericArray<User> =[
+const userList: GenericArray<User> = [
     {
-        name : "Maisa Monoara Moule",
+        name: "Maisa Monoara Moule",
+        age: 21
+    }
+]
+
+console.log(userList)
+
+// Generic with Interface
+
+interface IUser {
+    name: string,
+    age: number
+}
+
+const secondUser: GenericArray<IUser> = [
+    {
+        name: 'Abdulla Al Noman',
         age : 21
     }
 ]
+
+console.log(secondUser)
