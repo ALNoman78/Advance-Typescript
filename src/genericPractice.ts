09b7aab95dac2e7197dbb6908dd469c2b6a0c76b
ALNoman78 <abdullaalno078@gmail.com>
2025-12-09 23:11:44 +0600

    practice day 12

--- src/genericPractice.ts
+++ src/genericPractice.ts
@@ -1,43 +1,43 @@
-const GenericDeveloper = <X,Y>(para1 :X, para2:Y) => {
-    return [para1 , para2]
+const GenericDeveloper = <X, Y>(para1: X, para2: Y) => {
+    return [para1, para2]
 }
 
-const arrObject = GenericDeveloper('Abdullah al noman' , 21)
+const arrObject = GenericDeveloper('Abdullah al noman', 21)
 
 console.log(arrObject)
 
-const genericConstructive = <T> (course: T) =>{
+const genericConstructive = <T>(course: T) => {
     return [
-    {
-        course : 'Next Level Web Development',
-        courseFee : 6500,
-        ...course
-    }
+        {
+            course: 'Next Level Web Development',
+            courseFee: 6500,
+            ...course
+        }
     ]
 }
 
 type user = {
-    name : string;
-    uid : number;
+    name: string;
+    uid: number;
     phoneNumber?: number;
     hasCar?: boolean;
-    isMarried? : boolean;
-    hasJob? : boolean
+    isMarried?: boolean;
+    hasJob?: boolean
 }
 
-const student1 : user = {
-    name : 'Maisa Monoara Moule',
-    uid : 212,
-    hasCar : false
+const student1: user = {
+    name: 'Maisa Monoara Moule',
+    uid: 212,
+    hasCar: false
 }
 
-const student2 : user = {
-    name : 'abdulla al noman',
-    uid : 213,
-    hasCar : true,
-    hasJob : true,
-    isMarried : false,
-    phoneNumber : 01318969328,
+const student2: user = {
+    name: 'abdulla al noman',
+    uid: 213,
+    hasCar: true,
+    hasJob: true,
+    isMarried: false,
+    phoneNumber: 01318969328,
 }
 
 console.log(student1)
@@ -48,10 +48,23 @@ const mainFunction = <T>(param1: T) => {
     return [param1]
 }
 
-console.log(mainFunction('abdulla al noman mondol'))
+// console.log(mainFunction('abdulla al noman mondol'))
 
-console.log(mainFunction({
-    name: 'abudllah al noman ',
-    number: '01318-9969328',
-    id: 21
+// console.log(mainFunction({
+//     name: 'abudllah al noman ',
+//     number: '01318-9969328',
+//     id: 21
+// }))
+
+const secFunction = <T>(param2: T) => {
+    return [{
+        course: 'Next Level Development',
+        ...param2
+    }]
+}
+
+
+console.log(secFunction({
+    name: 'Abdulla Al Noman',
+    id: 123,
 }))
