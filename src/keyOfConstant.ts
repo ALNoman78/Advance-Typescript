d624006f971d6ef8256f9cdc5214c0fa6aaa594f
ALNoman78 <abdullaalno078@gmail.com>
2025-12-12 22:00:28 +0600

    key of constant

--- src/keyOfConstant.ts
+++ src/keyOfConstant.ts
@@ -12,6 +12,32 @@ type MyVehicle1 = 'bike' | 'car' | 'cng'
 
 type MyVehicle2 = keyof RichPeoplesVehicle;
 
-const myVehicle : MyVehicle2 = "bike"
+const myVehicle: MyVehicle2 = "bike"
 
-console.log(myVehicle)
+// console.log(myVehicle)
+
+// TODO User object 
+
+const user = {
+    id: 12,
+    name: 'Moule',
+    address: {
+        city: 'Barguna'
+    },
+}
+
+// const myName = user.id
+
+const myName = user['name']
+const myId = user['id']
+const myAddress = user['address']
+
+
+// console.log({ myName, myAddress, myId })
+
+const getResultFromObj = (obj: object, key: string) => {
+    return obj[key]
+}
+
+const result = getResultFromObj(user, 'name')
+console.log(result)
