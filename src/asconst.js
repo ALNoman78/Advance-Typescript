7d19539c6a4f535f0c4cd1060b6f67beaf9e64e9
ALNoman78 <abdullaalno078@gmail.com>
2025-12-16 23:18:57 +0600

    as const new enum use case added

--- src/asconst.js
+++ src/asconst.js
@@ -1,30 +1,24 @@
 //  todo as const assertion
-
 // enum UserRole {
 //     Admin = "Admin",
 //     Editor = "Editor",
 //     Viewer = "Viewer"
 // }
-
-
 // todo use there javascript object
-
-const UserRole = {
+var UserRole = {
     Admin: "ADMIN",
     Editor: "EDITOR",
     Viewer: "VIEWER"
-} as const;
-
-const canEdit = (role: (typeof UserRole[keyof typeof UserRole])) => {
+};
+var canEdit = function (role) {
     if (role === UserRole.Admin || role === UserRole.Editor) {
         return true;
-    } else {
+    }
+    else {
         return false;
     }
-}
-
-const isEditPermissible = canEdit(UserRole.Admin)
-console.log(isEditPermissible)
-
+};
+var isEditPermissible = canEdit(UserRole.Admin);
+console.log(isEditPermissible);
 // 1. use typeof operator
 // 2. use keyof operator
