1f1771d86fd74637aa39689c66f301389c04c66d
ALNoman78 <abdullaalno078@gmail.com>
2025-12-19 00:05:55 +0600

    practice as const

---
+++ src/practiceAsConst.ts
@@ -0,0 +1,16 @@
+const UserRole = {
+    Admin : "Admin",
+    Editor : "Editor",
+    Viewer : "Viewer"
+} as const;
+
+const canEdit = (role : (typeof UserRole [ keyof typeof UserRole]) ) => {
+    if (role === UserRole.Admin || role === UserRole.Editor) {
+        return true
+    }else{
+        return false;
+    }
+}
+
+const result = canEdit(UserRole.Admin)
+console.log(result)
