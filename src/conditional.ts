73190b809eb7071a02a1c752fd6d2073fd9f7f3c
ALNoman78 <abdullaalno078@gmail.com>
2025-12-20 23:15:59 +0600

    practice condition with generic

--- src/conditional.ts
+++ src/conditional.ts
@@ -1,17 +1,30 @@
-// what is conditional type : those type based on condition
+// // what is conditional type : those type based on condition
 
-type A = null;
-type B = undefined;
+// type A = null;
+// type B = undefined;
 
-type C = A extends null ? true : B extends undefined ? true : false
+// type C = A extends null ? true : B extends undefined ? true : false
+
+
+// type RichPeopleVehicle = {
+//     bike: string;
+//     car: string;
+//     ship: string
+// }
+
+// type CheckVehicle<T> = T extends keyof RichPeopleVehicle ? true : false
+
+// type HasBike = CheckVehicle<"bike">
+
+
 
 
 type RichPeopleVehicle = {
     bike: string;
     car: string;
-    ship: string
+    ship: string;
 }
 
-type CheckVehicle<T> = T extends "bike" | "car" | "ship" ? true : false
+type CheckVehicle<T> = T extends keyof RichPeopleVehicle ? true : false
 
-type HasBike = CheckVehicle<"ship">
+type HasBike = CheckVehicle<"car">
