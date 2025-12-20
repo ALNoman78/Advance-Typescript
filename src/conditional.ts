44b86ea0667d00a61b6488fc04e222c80158b03f
ALNoman78 <abdullaalno078@gmail.com>
2025-12-20 23:11:16 +0600

    condition with generic

--- src/conditional.ts
+++ src/conditional.ts
@@ -1,6 +1,17 @@
 // what is conditional type : those type based on condition
 
 type A = null;
-type b = undefined;
+type B = undefined;
 
-type C = A extends null ? true : false;
+type C = A extends null ? true : B extends undefined ? true : false
+
+
+type RichPeopleVehicle = {
+    bike: string;
+    car: string;
+    ship: string
+}
+
+type CheckVehicle<T> = T extends "bike" | "car" | "ship" ? true : false
+
+type HasBike = CheckVehicle<"ship">
