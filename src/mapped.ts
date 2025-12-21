93f848909934698f32c23191324f7651a768b43f
ALNoman78 <abdullaalno078@gmail.com>
2025-12-21 23:15:05 +0600

    mapped added

---
+++ src/mapped.ts
@@ -0,0 +1,11 @@
+// todo Mapped types
+
+// map
+
+const arrOfNumber: number[] = [1, 2, 3, 4, 6]
+
+const arrOfString: string[] = ['1', '2', '3', '4', '6'];
+
+const arrayOfStringUsingMap: string[] = arrOfNumber.map((num) => num.toString() )
+
+console.log(arrOfNumber)
