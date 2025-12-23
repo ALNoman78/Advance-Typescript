c41d4929e9f50c3d8438b585d68837cdcc6ae018
ALNoman78 <abdullaalno078@gmail.com>
2025-12-23 23:10:46 +0600

    mappedn ts

--- src/mapped.ts
+++ src/mapped.ts
@@ -9,3 +9,5 @@ const arrOfString: string[] = ['1', '2', '3', '4', '6'];
 const arrayOfStringUsingMap: string[] = arrOfNumber.map((num) => num.toString() )
 
 console.log(arrOfNumber)
+
+// todo exploring Mapped in typescript
